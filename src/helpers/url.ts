0396545006f19851d88b72605952d7276b5b5bcd
cumelmell <cumelmell@foxmail.com>
2019-09-28 01:10:08 +0800

    feat(index): handle and build URL
    - handle params and build URL
    - provide URL handler util

---
+++ src/helpers/url.ts
@@ -0,0 +1,78 @@
+import { isDate, isObject } from './util'
+
+/**
+ * 对url参数进行编码,针对部分特殊字符进行还原
+ * @param {string} val
+ * @return {string} encodeRes
+ */
+function encode(val: string): string {
+  return (
+    encodeURIComponent(val)
+      .replace(/%40/g, '@')
+      .replace(/%3A/gi, ':')
+      .replace(/%24/g, '$')
+      .replace(/%2c/gi, ',')
+      // 处理空格为+
+      .replace(/%20/g, '+')
+      .replace(/%5B/gi, '[')
+      .replace(/%5D/gi, ']')
+  )
+}
+
+/**
+ * 根据参数构造url
+ * @param {string} url
+ * @param {*} params
+ * @return {string} url
+ */
+export function buildURL(url: string, params?: any): string {
+  // 没有请求参数的话直接返回url
+  if (!params) {
+    return url
+  }
+
+  // 定义参数key=val键值对数组
+  const parts: string[] = []
+
+  // 遍历参数
+  Object.keys(params).forEach(key => {
+    const value = params[key]
+    // 对于null及undefined的val直接忽略
+    if (value === null || typeof value === 'undefined') {
+      return
+    }
+    let values = []
+    // 数组形式的,拼接成k[]=v&k[]=v
+    if (Array.isArray(value)) {
+      values = value
+      key += '[]'
+    } else {
+      values = [value]
+    }
+    values.forEach(val => {
+      // 处理Date类型
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        // 处理对象
+        val = JSON.stringify(val)
+      }
+      // 将key与valencode
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  // 序列化参数
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    // 处理hash
+    const markIndex = url.indexOf('#')
+    if (markIndex > -1) {
+      url.slice(0, markIndex)
+    }
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
